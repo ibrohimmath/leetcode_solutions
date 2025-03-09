de630551656d155542f6290de5f6c9450bee3df1
Ibrohim <113263072+ibrohimmath@users.noreply.github.com>
2025-03-09 12:25:08 +0500

    Time: 83 ms (80%), Space: 68.2 MB (40%) - LeetHub

--- 3483-alternating-groups-ii/3483-alternating-groups-ii.ts
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.ts
@@ -5,7 +5,6 @@ function numberOfAlternatingGroups(colors: number[], k: number): number {
         } else {
             return pref[r];
         }
-        // return pref[r] - pref[l] + Number(l > 0 ? colors[l] !== colors[l - 1] : 1);
     }
     
     const n: number = colors.length;
@@ -22,15 +21,10 @@ function numberOfAlternatingGroups(colors: number[], k: number): number {
     let counter: number = 0;
     for (let i = 0; i < n; i++) {
         if (i >= k - 1) {
-            // console.log(i - k + 1, i, rangeCount(i - k + 1, i));
             counter += Number(rangeCount(i - k + 1, i) === k);
         } else {
-            // console.log(colors.at(0), colors.at(-1));
-            // console.log(0, i, rangeCount(0, i));
-            // console.log(n - k + i + 1, n - 1, rangeCount(n - k + i + 1, n - 1));
             counter += Number(colors.at(0) !== colors.at(-1) && rangeCount(0, i) + rangeCount(n - k + i + 1, n - 1) === k);
         }
-        // console.log(counter, "###");
     }
 
     return counter;
