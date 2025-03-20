68bddf6d96a857195aa389814020973fdf581d45
Ibrohim <113263072+ibrohimmath@users.noreply.github.com>
2025-03-21 00:39:41 +0500

    Time: 12 ms (60%), Space: 66.5 MB (40%) - LeetHub

---
+++ 1037-minimum-number-of-k-consecutive-bit-flips/1037-minimum-number-of-k-consecutive-bit-flips.ts
@@ -0,0 +1,25 @@
+function minKBitFlips(nums: number[], k: number): number {
+    const n: number = nums.length; 
+    let flipped: boolean[] = Array(n).fill(false);
+
+    let flipSm: number = 0, counter: number = 0;
+    for (let i = 0; i < n; i++) {
+        if (i - k >= 0) {
+            flipSm -= +flipped[i - k];
+        }
+        if (flipSm & 1) {
+            nums[i] = 1 - nums[i];
+        }
+        if (i <= n - k && !nums[i]) {
+            nums[i] = 1 - nums[i];
+            flipped[i] = true;
+            flipSm++;
+            counter++;
+        }
+        if (nums[i] === 0) {
+            return -1;
+        }
+    }
+
+    return counter; 
+};
