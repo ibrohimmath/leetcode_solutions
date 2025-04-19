b737e93ed0920d9cc72625973aaba3bb9993c087
Ibrohim <113263072+ibrohimmath@users.noreply.github.com>
2025-04-19 13:34:00 +0500

    Time: 22 ms (99.09%), Space: 60.3 MB (97.26%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
@@ -8,17 +8,11 @@ public:
                 l++;
             }
             ans += min(l, i);
-            // cout << l << ' ' << i << ' ' << target << "\n"; 
         }
-        // cout << "###\n";
         return ans;
     }
     long long countFairPairs(vector<int> &nums, int lower, int upper) {
         sort(nums.begin(), nums.end()); 
-        // for (const int &item : nums) {
-        //     cout << item << " ";
-        // }
-        // cout << "\n";
         return countValid(nums, upper) - countValid(nums, lower - 1);
     }
 };
