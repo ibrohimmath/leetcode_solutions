7f227cbeaae6428712f6b8c54650e13edad4e9ba
Ibrohim <113263072+ibrohimmath@users.noreply.github.com>
2025-04-21 15:09:39 +0500

    Time: 12 ms (0%), Space: 73.7 MB (25%) - LeetHub

---
+++ 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.ts
@@ -0,0 +1,19 @@
+function numberOfArrays(differences: number[], lower: number, upper: number): number {
+    let item: number = 0, mn: number = 0, mx: number = 0; 
+    for (const difference of differences) {
+        item += difference;
+        mn = Math.min(mn, item);
+        mx = Math.max(mx, item);
+    }
+    console.log(mn, mx);
+    if (mx - mn > upper - lower) {
+        return 0;
+    }
+    if (mn <= lower) {
+        return upper - (mx + lower - mn - 1);  
+    } else if (mx >= upper) {
+        return (mn - (mx - upper) + 1) - lower;
+    } else {
+        return (mn - lower) + (upper - mx + 1);
+    }
+};
