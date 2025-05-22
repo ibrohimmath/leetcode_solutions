81e747033358ba5ef497670b0197a43acf36e801
Ibrohim <113263072+ibrohimmath@users.noreply.github.com>
2025-05-22 12:22:51 +0500

    Time: 3 ms (70.41%), Space: 60 MB (60.45%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.ts
@@ -0,0 +1,43 @@
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+function setZeroes(matrix: number[][]): void {
+    const m = matrix.length, n = matrix[0].length;
+    let firstRow = false, firstCol = false;
+    for (let i = 0; i < m; i++) {
+        if (matrix[i][0] == 0) {
+            firstCol = true;
+            break;
+        }
+    }
+    for (let j = 0; j < n; j++) {
+        if (matrix[0][j] == 0) {
+            firstRow = true;
+            break;
+        }
+    }
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (matrix[i][j] == 0) {
+                matrix[i][0] = matrix[0][j] = 0;
+            }
+        }
+    }
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+    if (firstRow) {
+        for (let j = 0; j < n; j++) {
+            matrix[0][j] = 0;
+        }
+    } 
+    if (firstCol) {
+        for (let i = 0; i < m; i++) {
+            matrix[i][0] = 0;
+        }
+    }
+};
