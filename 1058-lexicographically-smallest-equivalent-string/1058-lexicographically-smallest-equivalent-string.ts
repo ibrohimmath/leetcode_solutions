92784698d6ad1f85ca95553244ce76ea026d7165
Ibrohim <113263072+ibrohimmath@users.noreply.github.com>
2025-06-06 02:16:28 +0500

    Time: 8 ms (50%), Space: 60.4 MB (0%) - LeetHub

---
+++ 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.ts
@@ -0,0 +1,43 @@
+function smallestEquivalentString(s1: string, s2: string, baseStr: string): string {
+    function getCode(x: string) {
+        return x.charCodeAt(0) - 'a'.charCodeAt(0);
+    }
+    function getAlpha(x: number) {
+        return String.fromCharCode(x + 'a'.charCodeAt(0));
+    }
+
+    class DSU {
+        public parent: number[];
+        public value: number[];
+
+        constructor() {
+            this.parent = new Array(26).fill(-1).map((_, i) => i);
+        }
+        
+        find(node: number) {
+            if (node == this.parent[node]) return node;
+            return this.parent[node] = this.find(this.parent[node]);
+        }
+
+        unite(a: number, b: number) {
+            let pa: number = this.find(a), pb: number = this.find(b);
+            if (pa == pb) {
+                return;
+            }
+            if (pa > pb) {
+                [pa, pb] = [pb, pa];
+            }
+            this.parent[pb] = pa;
+        }
+    }
+
+    const dsu = new DSU();
+    for (let i = 0; i < s1.length; i++) {
+        const code1: number = getCode(s1[i]), code2: number = getCode(s2[i]);
+        dsu.unite(code1, code2);
+    }
+    // for (let i = 0; i < 26; i++) {
+    //     console.log(getAlpha(i), getAlpha(dsu.parent[i]));
+    // }
+    return baseStr.split('').map(c => dsu.find(getCode(c))).map(getAlpha).join(''); 
+};
