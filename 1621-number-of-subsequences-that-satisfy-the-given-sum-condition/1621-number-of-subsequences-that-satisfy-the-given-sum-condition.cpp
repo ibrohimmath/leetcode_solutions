d293fd08a7137119ef69875128480feb183764b6
Ibrohim <113263072+ibrohimmath@users.noreply.github.com>
2025-06-29 20:29:38 +0500

    Time: 38 ms (26.66%), Space: 51.5 MB (95.04%) - LeetHub

---
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int MOD = 1e9 + 7;
+
+    int binpow(int a, int n) {
+        if (a == 0) {
+            return 0;
+        }
+        if (n == 0) {
+            return 1;
+        }
+        int res = 1;
+        while(n > 0) {
+            if (n & 1) {
+                res = int(1LL * res * a % MOD);
+            }
+            a = int(1LL * a * a % MOD);
+            n >>= 1;
+        }
+        return res;
+    }
+    
+    int numSubseq(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        const int n = nums.size();
+        int ans = 0;
+        for (int l = 0, r = n - 1; r >= 1; r--) {
+            while (l < r && nums[l] + nums[r] <= target) {
+                ans = (ans + binpow(2, r - l) - 1) % MOD;
+                l++;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+           ans = (ans + (2 * nums[i] <= target)) % MOD;
+        }
+        return ans;
+    }
+};
