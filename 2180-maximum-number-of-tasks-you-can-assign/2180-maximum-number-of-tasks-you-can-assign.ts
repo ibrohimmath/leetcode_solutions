6790987fd2ce2b27949074f626d3950bc55bc4ea
Ibrohim <113263072+ibrohimmath@users.noreply.github.com>
2025-07-17 01:51:14 +0500

    Time: 120 ms (100%), Space: 73.1 MB (41.67%) - LeetHub

--- 2180-maximum-number-of-tasks-you-can-assign/2180-maximum-number-of-tasks-you-can-assign.ts
+++ 2180-maximum-number-of-tasks-you-can-assign/2180-maximum-number-of-tasks-you-can-assign.ts
@@ -1,14 +1,11 @@
 function maxTaskAssign(tasks: number[], workers: number[], pills: number, strength: number): number {
     function canTasksDone(tasksLen: number) {
-        // console.log(`Case when tasks ${tasksLen}`);
         let l: number = 0, r: number = 0;
         const queue: number[] = Array(tasksLen).fill(-1);
 
         let counter: number = 0;
         for (let i = wLen - Math.min(wLen, tasksLen), j = 0, p = pills; i < wLen; i++) {
-            // console.log("worker", workers[i]);
             while (j < tasksLen && tasks[j] <= workers[i] + strength) {
-                // console.log("task", tasks[j]);
                 queue[r] = tasks[j];
                 r++;
                 j++;
@@ -17,11 +14,9 @@ function maxTaskAssign(tasks: number[], workers: number[], pills: number, streng
             if (queue.length == 0) continue;
 
             if (l < r && queue.at(l) <= workers[i]) {
-                // console.log("Chosen task", queue.at(l));
                 l++;
                 counter++;
             } else if (p > 0 && l < r) {
-                // console.log("Chosen task with pill", queue.at(-1));
                 r--; 
                 p--;
                 counter++;
