0a3c86df3be6b566f676a9dadd35ab320dc259dd
Ibrohim <113263072+ibrohimmath@users.noreply.github.com>
2025-09-12 12:25:54 +0500

    Time: 11 ms (88.89%), Space: 59.3 MB (100%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.ts
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.ts
@@ -2,8 +2,8 @@ function doesAliceWin(s: string): boolean {
     const vowels: string = 'aeiuo';
     let count: number = 0;
     for (let i = 0; i < s.length; i++) {
-        count += Number(vowels.includes(s[i]));
+        if (vowels.includes(s[i]))
+            return true;
     } 
-    if (count == 0) return false;
-    return true;
+    return false;
 };
