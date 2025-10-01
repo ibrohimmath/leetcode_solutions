99c538dc112929cec26e78020765d1d2bb40f678
Ibrohim <113263072+ibrohimmath@users.noreply.github.com>
2025-10-02 01:27:47 +0500

    Time: 7 ms (65.41%), Space: 54.1 MB (99.42%) - LeetHub

--- 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.ts
+++ 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.ts
@@ -1,26 +1,16 @@
 function minScoreTriangulation(values: number[]): number {
     const n: number = values.length;
-    const dp: number[][] = Array.from({ length: n }, () => Array(n).fill(1e18));
+    const dp: number[][] = Array.from({ length: n }, () => Array(n).fill(0));
 
-    for (let i = 0; i < n; i++) {
-        for (let d: number = 0; d < 2 && i + d < n; d++) {
-            dp[i][i + d] = 0;
-        }    
-    }
-
-    let mn: number = 1e18;
     for (let d = 2; d < n; d++) {
         for (let i = 0; i + d < n; i++) {
             const j: number = i + d;
 
+            let mn: number = 1e18;
             for (let k = i + 1; k < j; k++) {
-                dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k][j] + values[i] * values[j] * values[k]);
-                // console.log(i, j, dp[i][j], "main");
-                // console.log(i, k, dp[i][k]);
-                // console.log(k, j, dp[k][j]);
-                // console.log(i, j, k, values[i], values[j], values[k]);
-                // console.log("#######");
+                mn = Math.min(mn, dp[i][k] + dp[k][j] + values[i] * values[j] * values[k]);
             }
+            dp[i][j] = mn;
         }
     }
 
