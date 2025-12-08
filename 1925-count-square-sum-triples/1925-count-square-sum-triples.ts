3eed3e3e1c0c26f3ec7ff8b1e733458cb507867f
Ibrohim <113263072+ibrohimmath@users.noreply.github.com>
2025-12-09 00:42:59 +0500

    Time: 1 ms (100%), Space: 55.4 MB (80%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.ts
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.ts
@@ -1,10 +1,16 @@
 function countTriples(n: number): number {
+    function gcd(x: number, y: number) {
+        if (y == 0) return x;
+        return gcd(y, x % y);
+    }
+
     let ans: number = 0;
-    for (let i = 1; i <= n; i++) {
-        for (let j = 1; j < i; j++) {
-            if (Math.floor(Math.sqrt(i * i - j * j)) == Math.sqrt(i * i - j * j)) {
-                ans++;
-            }
+    for (let i = 1; i * i <= n; i++) {
+        for (let j = 1 + (i % 2); j < i; j += 2) {
+            if (gcd(i, j) != 1) continue;
+            const c: number = i * i + j * j;
+            if (c > n) continue;
+            ans += 2 * Math.floor(n / c);
         }
     }
     return ans;
